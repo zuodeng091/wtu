27033c2860d45449e2c2e727fa94206d1da08437
zuodeng <1125280065@qq.com>
2015-07-20 16:51:15 +0800

    basemapper修改

--- wtu-tbdata-dal/src/main/java/com/wtu/tbdata/dal/mapper/BaseMapper.java
+++ wtu-tbdata-dal/src/main/java/com/wtu/tbdata/dal/mapper/BaseMapper.java
@@ -1,7 +1,40 @@
 package com.wtu.tbdata.dal.mapper;
 
+import com.wtu.tbdata.common.spring.SpringContextHolder;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 定义sqlsession
+ * @author zuodeng 作者
+ */
 public class BaseMapper {
-	
-	
+
+    /**
+     * 加入日志
+     */
+    private static Logger logger = LoggerFactory.getLogger(BaseMapper.class);
+
+    /**
+     * sqlSession
+     */
+    protected static SqlSession sqlSession;
+
+    /**
+     * 加载类的时候初始化sqlSession
+     */
+    static {
+        SqlSessionFactory sqlSessionFactory = SpringContextHolder.getBean("sqlSessionFactory");
+        if(sqlSessionFactory == null){
+            logger.info("spring加载sqlSessionFactory失败");
+        }else{
+            sqlSession = sqlSessionFactory.openSession();
+            if(sqlSession != null){
+                logger.info("spring成功加载sqlSession");
+            }
+        }
+    }
 
 }
--- wtu-tbdata-dal/src/main/java/com/wtu/tbdata/dal/mapper/impl/WtuUserMapperImpl.java
+++ wtu-tbdata-dal/src/main/java/com/wtu/tbdata/dal/mapper/impl/WtuUserMapperImpl.java
@@ -2,9 +2,6 @@ package com.wtu.tbdata.dal.mapper.impl;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Repository;
 
 import com.wtu.tbdata.dal.mapper.BaseMapper;
@@ -14,62 +11,48 @@ import com.wtu.tbdata.domain.WtuUserExample;
 
 @Repository("wtuUserMapper")
 public class WtuUserMapperImpl extends BaseMapper implements WtuUserMapper{
-
-	@Resource
-	private SqlSessionFactory sqlSessionFactory;
-	
 	
 	public int countByExample(WtuUserExample example) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int deleteByExample(WtuUserExample example) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int deleteByPrimaryKey(Long id) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int insert(WtuUser record) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int insertSelective(WtuUser record) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public List<WtuUser> selectByExample(WtuUserExample example) {
-		return sqlSessionFactory.openSession().selectList("WtuUserMapper.selectByExample", example);
+		return sqlSession.selectList("WtuUserMapper.selectByExample", example);
 	}
 
 	public WtuUser selectByPrimaryKey(Long id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public int updateByExampleSelective(WtuUser record, WtuUserExample example) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int updateByExample(WtuUser record, WtuUserExample example) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int updateByPrimaryKeySelective(WtuUser record) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int updateByPrimaryKey(WtuUser record) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
